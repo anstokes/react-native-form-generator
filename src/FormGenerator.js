48ab9e592fb12caed64abed5d8065da3989a9919
Nicolae Ifrim <nicolae.ifrim@sysx.co.uk>
2021-02-08 12:26:10 +0000

    Theme forwarded to children, and updated README.

--- src/FormGenerator.js
+++ src/FormGenerator.js
@@ -150,6 +150,7 @@ const FormGenerator = ({ schema, library, submitHandler, theme, navigation, vali
                                     )
                                 })}
 
+
                                 {/* Current screen form elements */}
                                 {typeof values === 'object' && Object.keys(values).map(fieldName => {
                                     let fieldProperties = formProperties[currentScreen][fieldName];
@@ -166,9 +167,12 @@ const FormGenerator = ({ schema, library, submitHandler, theme, navigation, vali
                                             disabled={!!(Array.isArray(customValidation.disabled) && customValidation.disabled.includes(fieldName)) || isSubmitting}
                                             {...fieldProperties.props}
                                             library={library}
+                                            theme={theme}
                                         />
                                     ) : null;
                                 })}
+
+
                                 {/* Current screen actions */}
                                 <View style={styles.actionsWrapper}>
                                     {schema.screens[currentScreen].actions && Object.keys(schema.screens[currentScreen].actions).map(actionName => {
@@ -187,6 +191,7 @@ const FormGenerator = ({ schema, library, submitHandler, theme, navigation, vali
                                                 navigateTo={button.navigateTo ? button.navigateTo : currentScreen}
                                                 label={button.label ? button.label : 'Missing Label'}
                                                 library={library ? library : {}}
+                                                theme={theme}
                                                 type={button.type ? button.type : 'submit'}
                                                 action={button.action ? button.action : 'submit'}
                                                 errors={errors}
